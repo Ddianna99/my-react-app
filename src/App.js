a066eb6cf6e2f390c798f010eaa4868057ceeefa
Diana Dumitrescu <ddiana.dumitrescu@gmail.com>
2022-09-02 11:25:03 +0300

    second commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
+import Message from './componets/Message';
 import Profile from './componets/Profile';
 
 function App() {
   return (
     <div className="App">
     <Profile name="Diana"></Profile>
+    <Message message = "Hello!"/>
     </div>
   );
 }
